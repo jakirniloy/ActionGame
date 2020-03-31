0bc8603fc3b8c9bea91fb1d9e614497cfb4f7152
Jakir Hossain Niloy <48614476+jakirvaw@users.noreply.github.com>
2020-03-31 15:14:55 +0600

    Action Game 2d

--- app/src/main/java/com/example/actiongame/GameActivity.java
+++ app/src/main/java/com/example/actiongame/GameActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Point;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 private GameView gameview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Point point = new Point();
         getWindowManager().getDefaultDisplay().getSize(point);
         gameview = new GameView(this,point.x,point.y);
--- app/src/main/java/com/example/actiongame/MainActivity.java
+++ app/src/main/java/com/example/actiongame/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.actiongame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+     findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             startActivity(new Intent(MainActivity.this,GameActivity.class));
+         }
+     });
     }
 }
